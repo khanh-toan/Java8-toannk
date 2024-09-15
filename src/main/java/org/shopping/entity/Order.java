a582eb0d1b1aa3159c6e1d281950e4d700ec0ebc
ADMIN-PC\DELL <toandoitnow11@gmail.com>
2024-09-15 20:38:39 +0700

    update code

--- src/main/java/org/shopping/entity/Order.java
+++ src/main/java/org/shopping/entity/Order.java
@@ -1,13 +1,18 @@
 package org.shopping.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "Orders", //
         uniqueConstraints = { @UniqueConstraint(columnNames = "Order_Num") })
+@Data
 public class Order implements Serializable {
+    @Serial
     private static final long serialVersionUID = -2576670215015463100L;
     @Id
     @Column(name = "id", length = 50)
@@ -27,53 +32,4 @@ public class Order implements Serializable {
     private String customerEmail;
     @Column(name = "Customer_Phone", length = 128, nullable = false)
     private String customerPhone;
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public Date getOrderDate() {
-        return orderDate;
-    }
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-    public int getOrderNum() {
-        return orderNum;
-    }
-    public void setOrderNum(int orderNum) {
-        this.orderNum = orderNum;
-    }
-    public double getAmount() {
-        return amount;
-    }
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-    public String getCustomerName() {
-        return customerName;
-    }
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getCustomerAddress() {
-        return customerAddress;
-    }
-    public void setCustomerAddress(String customerAddress) {
-        this.customerAddress = customerAddress;
-    }
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-    public String getCustomerPhone() {
-        return customerPhone;
-    }
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
-    }
 }
